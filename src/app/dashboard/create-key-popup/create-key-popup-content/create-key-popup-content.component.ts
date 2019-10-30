d1c23312bcb393fa5adb6af11d161004499b112e
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-10-30 16:25:06 +0530

    made createkey popup fields more dynamic...as it is taking data from an url

--- src/app/dashboard/create-key-popup/create-key-popup-content/create-key-popup-content.component.ts
+++ src/app/dashboard/create-key-popup/create-key-popup-content/create-key-popup-content.component.ts
@@ -1,7 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,Input } from '@angular/core';
 import {CreateNewKeyService} from '../../../services/create-new-key.service'
 import {KeyList} from '../../../shared/keyList.model'
 import {NewClient} from '../../../shared/newClient.model'
+import {NonCapiClient} from '../../../shared/nonCapiClient.model'
 import { CreateKeyAlertComponent } from '../create-key-alert/create-key-alert.component';
 import { Alert } from 'selenium-webdriver';
 
@@ -11,29 +12,40 @@ import { Alert } from 'selenium-webdriver';
   styleUrls: ['./create-key-popup-content.component.css']
 })
 export class CreateKeyPopupContentComponent implements OnInit {
+  @Input() nonCapiClientList:NonCapiClient[];
   constructor(private NewKey:CreateNewKeyService) { }
   newAccessKey:KeyList;
   bannerType:string;
   ngOnInit() {
+  }
+  requestApiToCreateKey(client,cpg,program,updatedBy){
+    this.NewKey.post(new NewClient(client,cpg,program,updatedBy))
+      .subscribe(
+        key => {
+          this.newAccessKey=key;
+        }
+      );
+  }
+  validateIfCreated(client,cpg,program,updatedBy){
+    console.log(this.newAccessKey);
+    this.bannerType="Creating";
+    if(this.newAccessKey)    
+      if((this.newAccessKey.clientName==client)&&(this.newAccessKey.updatedBy==updatedBy)&&(this.newAccessKey.iskeyActive==false)&&this.newAccessKey.accessKey)
+        this.bannerType="Success";
+      else
+        this.bannerType="Error";
   }
   createKey(event){
     var target=event.target
     const client=target.querySelector('#clientName').value
     const cpg=target.querySelector('#cpg').value
     const program=target.querySelector('#program').value
+    const updatedBy="CuttingChai";
     if((client=="")||(cpg=="")||(program==""))
     this.bannerType="Required";
     else{
-      this.NewKey.post(new NewClient(client,cpg,program,"ABCD"))
-      .subscribe(
-        key => {
-          this.newAccessKey=key;
-        }
-      );
-      if(!this.newAccessKey)
-        this.bannerType="Error";
-      else
-        this.bannerType="Success";
+      this.requestApiToCreateKey(client,cpg,program,updatedBy);
+      this.validateIfCreated(client,cpg,program,updatedBy);
     }
   }
 }
