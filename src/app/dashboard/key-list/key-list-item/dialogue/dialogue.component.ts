a3faaf8d579e0ffb3899e9779c37b210f09339e8
Soumyadeep <51915541+tavisca-sjana@users.noreply.github.com>
2019-11-03 13:18:59 +0530

    Added change state service

--- src/app/dashboard/key-list/key-list-item/dialogue/dialogue.component.ts
+++ src/app/dashboard/key-list/key-list-item/dialogue/dialogue.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit, EventEmitter, Output, Input } from '@angular/core';
+import { ChangeKeyStateService } from 'src/app/services/change-key-state.service';
+import { KeyList } from 'src/app/shared/KeyList.model';
 
 @Component({
   selector: 'app-dialogue',
@@ -9,14 +11,18 @@ export class DialogueComponent implements OnInit {
   @Output() toggleYes = new EventEmitter();
   @Input() id;
   @Input() keyItem;
+  @Output() updatedKeyItem = new EventEmitter<KeyList>();
   
-  constructor() { }
+  constructor(private changeKeyState: ChangeKeyStateService) { }
 
   ngOnInit() {
   }
 
   onClick() {
     // this.toggleYes.emit();
+    this.changeKeyState.onChangeKeyState(this.keyItem).subscribe((response) => {
+      this.updatedKeyItem.emit(response);
+    });
     console.log(this.keyItem);
   }
 
--- src/app/services/change-key-state.service.ts
+++ src/app/services/change-key-state.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { KeyList } from '../shared/KeyList.model';
+import { Observable } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ChangeKeyStateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  baseUrl = '';
+  endPoint = '';
+  onChangeKeyState(keyItem: KeyList): Observable<KeyList> {
+    this.endPoint = keyItem.iskeyActive ? 'deactivate' : 'activate';
+    let putUrl = `${this.baseUrl}/${this.endPoint}/${keyItem.accessKey}`;
+
+    return this.http.put<KeyList>(putUrl, keyItem);
+  }
+
 }
