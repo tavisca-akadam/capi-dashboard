13b0d5219b81411a937d033cfc81be1cb7942225
tavisca-dmandal <51920311+tavisca-dmandal@users.noreply.github.com>
2019-11-06 21:43:26 +0530

    modified program.model to old model version

--- src/app/shared/program.model.ts
+++ src/app/shared/program.model.ts
@@ -1,3 +1,3 @@
 export class Program {
-    constructor(public name:string,public id:string){}
+    constructor(public classicId:string,public name:string,public id:string,public createdOn:string,public lastUpdatedOn:string){}
 }
