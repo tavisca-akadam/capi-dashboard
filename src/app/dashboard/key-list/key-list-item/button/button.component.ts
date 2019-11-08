41f6a0bf64df5e87745ebde2389fac11ea49c2a3
tavisca-apandey <apandey@tavisca.in>
2019-11-08 11:49:00 +0530

    Added Toggle Button

--- src/app/dashboard/key-list/key-list-item/button/button.component.ts
+++ src/app/dashboard/key-list/key-list-item/button/button.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewChild, ElementRef } from '@angular/core';
 import { ChangeStateService } from 'src/app/services/change-state.service';
+import { ButtonStateService } from 'src/app/services/button-state.service';
 
 @Component({
   selector: 'app-button',
@@ -12,13 +13,28 @@ export class ButtonComponent implements OnInit {
   btnText: string;
   dataAttribute:string;
 
-  constructor(private ChangeAccessKeyStateService: ChangeStateService) {}
+  @ViewChild('stateToggle', { static: false }) stateToggle: ElementRef;
+
+  constructor(private ChangeAccessKeyStateService: ChangeStateService,
+              private buttonStateService: ButtonStateService) {}
 
   ngOnInit() {
     console.log(this.buttonState);
     this.dataAttribute = `#${this.id}`;
-    //console.log(this.id);
+
     //this.ChangeAccessKeyStateService.changeAccessKeyState(this.buttonState, this.id);
+  }
+
+  ngAfterViewInit() {
+    this.onStateChange();
+
+    this.buttonStateService.buttonState.subscribe((checked: boolean) => {
+      (this.stateToggle.nativeElement as HTMLInputElement).checked = checked;    
+    });
+  }
+
+  onStateChange() {
+    (this.stateToggle.nativeElement as HTMLInputElement).checked = this.buttonState;
   }
       
   toggleKeyState(event) {
