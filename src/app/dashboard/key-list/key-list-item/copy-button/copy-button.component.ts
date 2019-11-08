9b997649c697c1deb960bdaa0005b25fbb7c1c45
Tavisca-ajain <51915650+Tavisca-ajain@users.noreply.github.com>
2019-11-08 12:44:59 +0530

    Added copy button

---
+++ src/app/dashboard/key-list/key-list-item/copy-button/copy-button.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-copy-button',
+  templateUrl: './copy-button.component.html',
+  styleUrls: ['./copy-button.component.css']
+})
+export class CopyButtonComponent implements OnInit {
+  /* To copy any Text */
+  @Input() key;
+copyText(val: string){
+  let selBox = document.createElement('textarea');
+    selBox.style.position = 'fixed';
+    selBox.style.left = '0';
+    selBox.style.top = '0';
+    selBox.style.opacity = '0';
+    selBox.value = val;
+    document.body.appendChild(selBox);
+    selBox.focus();
+    selBox.select();
+    document.execCommand('copy');
+    document.body.removeChild(selBox);
+  }
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
